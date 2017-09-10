ea8717fa04ac88643a8b6dcc339be85bf2d30c00
Gerwazy <sikora.tomus@gmail.com>
2017-09-10 03:24:32 +0200

    Dodano rejestrację bez walidacji

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,15 +1,23 @@
 <?php 
 
 namespace App\Controllers;
+use App\Models\User;
 use Slim\Views\Twig as View;
+
 
 class HomeController extends Controller {
 
 
 		public function home($request, $response) {
-	//		return $this->view->render($response, 'home.twig');
-			$user = $this->db->table('users')->find(1);
-	    	var_dump($user->password); 
+			return $this->view->render($response, 'home.twig');
+			//$id = $request->getParam('id');
+			//$user = $this->db->table('users')->find($id);
+
+	    	//var_dump($user->name); 
+	    	//var_dump($user->email); 
+	    	//var_dump($user->password); 
+	    	//$user = User::where('email', 'tomeks@o2.pl')->first();
+			
 		}
 
 		public function about($request, $response) {
@@ -24,10 +32,4 @@ class HomeController extends Controller {
 
 }
 
-
-
-
-
-
-
- ?>
+?>
--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,8 @@
 <?php 
 
- 
-$app->get('/', 'HomeController:home');
+$app->get('/', 'HomeController:home')->setName('home');
 $app->get('/about', 'HomeController:about');
-
+$app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
+$app->post('/auth/signup', 'AuthController:postSignUp');
 
  ?>
